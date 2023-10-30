536711e36b29df2b2236187c3a0911135eac04d6
José Costa <68577870+carlosjuniorcosta1@users.noreply.github.com>
2023-10-29 22:30:25 -0300

    método get e método inserir frequência

--- src/ApiPython/apis_dir/api_frequencia.py
+++ src/ApiPython/apis_dir/api_frequencia.py
@@ -1,8 +1,7 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template, make_response
 import pyodbc
-import pandas as pd
 from flask_pydantic_spec import FlaskPydanticSpec
-
+import asyncio 
 
 app = Flask(__name__ )
 spec = FlaskPydanticSpec('flask', title = "Endpoints da tabela de avaliação")
@@ -16,31 +15,6 @@ data_for_connection = (
 )
 connection = pyodbc.connect(data_for_connection)
 cursor = connection.cursor()
-
-# cursor.execute(f"""create table tabela_frequencia (
-#    id_aluno int,
-#    id_materia int, 
-#    id_bimestre int,
-#    turma int ,
-#    data_aula datetime,
-#    descricao_aula nvarchar(150),
-#    habilidade_bncc text, 
-#    presente bit) """)
-
-# cursor.execute(f"""alter table tabela_frequencia add id_frequencia int identity 
-#                constraint PK_frequencia primary key clustered """) 
-
-# cursor.execute(f"""insert into tabela_frequencia (id_aluno, id_materia, id_bimestre,
-#                turma, data_aula, descricao_aula, habilidade_bncc,
-#                presente) values (
-#                   1, 6, 2, 1, 15-10-2023, 'atividade em sala', 'ler', 1
-#                ) 
-#                """)
-# db = cursor.execute(f"""select * from tabela_frequencia where turma = 2 and id_bimestre = 1
-#                     and id_materia = 2
-#                """) #inserir turma 2 bimestre 1 materia 2
-# db_l = db.fetchall()
-# print(db_l)
 
 @app.route('/diario/frequencia/<int:id_materia>/<int:id_bimestre>/<int:turma>', methods = ['GET'])
 def get_freq (id_materia, id_bimestre, turma):
@@ -60,12 +34,51 @@ def get_freq (id_materia, id_bimestre, turma):
                 'descricao_aula': x[5],
                 'habilidade_bncc': x[6],
                 'presente': x[7],
-                'id_frequencia': x[8]
-                
+                'id_frequencia': x[8]                
             }
         ) 
     return jsonify(message = "dados consultados", data = list_q)
 
-# @app.route('diario/frequencia/')
+@app.route('/diario/frequencia/inserir', methods = ["POST"])
+def insert_freq():
+    payload = request.get_json(force=True)
+    id_turma = payload[0].get('id_turma')
+    db_ids = cursor.execute(f"""SELECT id_aluno FROM tabela_alunos WHERE id_turma = {id_turma}""")
+    list_ids = db_ids.fetchall()
+    dic_ids = []
+    for x in list_ids:
+        for y in x:
+            dic_ids.append({
+                "id_aluno": y
+            })
+
+    result = []
+    num_alunos = len(dic_ids)
+    index = 0
+    for aluno in dic_ids:
+        data = payload[index % len(payload)]  
+        result.append({
+            "id_materia": data.get('id_materia'),
+            "id_bimestre": data.get('id_bimestre'),
+            "id_turma": id_turma,
+            "data_aula": data.get('data_aula'),
+            "descricao_aula": data.get('descricao_aula'),
+            "habilidade_bncc": data.get('habilidade_bncc'),
+            "id_aluno": aluno['id_aluno'],
+            "presente": data.get('presente')
+        })
+        index += 1
+    for x in result:
+        cursor.execute(f"""INSERT INTO tabela_frequencia (id_aluno, id_materia, id_bimestre, id_turma, data_aula,
+                       descricao_aula, habilidade_bncc, presente) VALUES (
+                           {x['id_aluno']}, {x['id_materia']}, {x['id_bimestre']}, 
+                           {id_turma}, '{x['data_aula']}', '{x['descricao_aula']}', '{x['habilidade_bncc']}',
+                           {x['presente']}       
+                       )
+                       """)
+    cursor.commit()               
+    
+    return jsonify(message= "está funcionando", data = result) 
+    
 
 app.run(debug=True)
